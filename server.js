4381cf93d1bcdff30817c1749343014f16e0e3d6
ColinRoberts <colinroberts96@gmail.com>
2018-06-05 16:24:10 +0400

    Started using heroku provided port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 var net = require('net');
+var HOST = '127.0.0.1';
+var PORT = 9001;
 
 var server = net.createServer(function(socket) {
 	socket.pipe(socket);
 });
 
-server.listen(9001, '127.0.0.1');
+server.listen(process.env.PORT || PORT, HOST);
